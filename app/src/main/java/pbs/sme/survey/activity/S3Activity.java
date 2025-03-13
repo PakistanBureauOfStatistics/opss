05a66518cc04c21d65c7eebc8265e80850fd9968
Zaheer <Zaheer@ZAHEER-PC>
2025-03-13 11:51:10 +0500

    Section 3, 8 and 9. Issues resolved. After saving data of section 3 it is not moving to section 4. Error in Section 4.

--- app/src/main/java/pbs/sme/survey/activity/S3Activity.java
+++ app/src/main/java/pbs/sme/survey/activity/S3Activity.java
@@ -1,208 +1,117 @@
 package pbs.sme.survey.activity;
 
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import pbs.sme.survey.R;
-import pbs.sme.survey.model.Block;
-import pbs.sme.survey.model.Constants;
-import pbs.sme.survey.model.Section12;
 import pbs.sme.survey.model.Section3;
-import pbs.sme.survey.utils.Utils;
+import pk.gov.pbs.utils.StaticUtils;
 
 public class S3Activity extends FormActivity {
+
     private Button sbtn;
     private List<Section3> modelDatabase;
-    Block mBlock;
-    Section12 section1;
-    List<Section3> resumeModel;
-    List<String> txtIds = Arrays.asList("Male","Female", "Wage", "Cash", "kind");
+
     private final String[] inputValidationOrder= new String[]{
-            "male","female", "wages", "other_cash_payment", "payment_in_kind"
+            //"value"
+           "male", "female", "wages", "other_cash_payment", "payment_in_kind"
+    };
+
+    private final String[] codeList= new String[]{
+            "301","302","303","304","305","306","307"//,"300"
     };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_s3);
-        setDrawer(this, "Section 3");
+        setDrawer(this,"Section 3: Emp Cost");
         setParent(this, S4Activity.class);
         scrollView = findViewById(R.id.scrollView);
-        mBlock = (Block) getIntent().getSerializableExtra(Constants.EXTRA.IDX_BLOCK);
-        section1 = (Section12) getIntent().getSerializableExtra(Constants.EXTRA.IDX_HOUSE);
-        assert section1 != null;
-        resumeModel = dbHandler.query(Section3.class, "uid=?", section1.uid);
-        resumeModel = resumeModel.isEmpty() ? null : resumeModel;
-     //   findViewById(R.id.btnp).setOnClickListener(MoveToPrevious());
-     //   findViewById(R.id.btns).setOnClickListener(SaveData());
-       // findViewById(R.id.btnn).setOnClickListener(MoveToNext());
-        if(resumeModel!=null)
-            loadFarmData();
+
+        //EditText totalEditText = findViewById(R.id.value__300);
+
+//        //AdditionTextWatcher additionTextWatcher = new AdditionTextWatcher(totalEditText);
+//
+//        for(int i = 0; i < codeList.length-1; i++) {
+//            EditText et = findViewById(getResources().getIdentifier("value__"+codeList[i], "id", getPackageName()));
+//            et.removeTextChangedListener(additionTextWatcher);
+//            et.addTextChangedListener(additionTextWatcher);
+//        }
+
+
+
+        sbtn = findViewById(R.id.btns);
+        sbtn.setOnClickListener(v -> {
+            sbtn.requestFocus();
+            StaticUtils.getHandler().post(this::saveForm);
+        });
+
     }
 
+    private void saveForm() {
+        sbtn.setEnabled(false);
+        List<Section3> list=new ArrayList<>();
 
+        for(int i = 0; i < codeList.length; i++) {
 
-    private void loadFarmData() {
-        for(Section3 s3 : resumeModel){
-            for(String txtId : txtIds){
-                EditText et = (EditText) findViewById(getResources().getIdentifier(txtId+s3.code, "id", getPackageName()));
-                switch (txtId){
-                    case "Male":
-                        et.setText(String.valueOf(s3.male));
-                        break;
-                    case "Female":
-                        et.setText(String.valueOf(s3.female));
-                        break;
-                    case "Wage":
-                        et.setText(String.valueOf(s3.wages));
-                        break;
-                    case "Cash":
-                        et.setText(String.valueOf(s3.other_cash_payment));
-                        break;
-                    case "kind":
-                        et.setText(String.valueOf(s3.payment_in_kind));
-                        break;
-                }
+            Section3 m = null;
+            if(modelDatabase != null && modelDatabase.size() == codeList.length){
+                m = modelDatabase.get(i);
             }
-//            EditText et = (EditText) findViewById(getResources().getIdentifier("Total"+s3.code, "id", getPackageName()));
-//            et.setText(String.valueOf(s3.Male + s3.Female));
+
+            try {
+                m = (Section3) extractValidatedModelFromForm(this, m, true, inputValidationOrder, codeList[i], Section3.class, false, this.findViewById(android.R.id.content));
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
+
+            if (m == null) {
+                mUXToolkit.showAlertDialogue("Failed", "فارم کو محفوظ نہیں کر سکتے، براہ کرم آگے بڑھنے سے پہلے تمام ڈیٹا درج کریں۔خالی اندراج یا غلط جوابات دیکھنے کے لیے \"OK\" پر کلک کریں۔", alertForEmptyFieldEvent);
+                sbtn.setEnabled(true);
+                return;
+            }
+
+            list.add(m);
+            setCommonFields(m);
+            m.section=3;
+            m.code=codeList[i];
+
         }
-    }
 
-    private View.OnClickListener MoveToNext() {
-        return new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-//                Intent intent = new Intent(getApplicationContext(), Section4Activity.class);
-//                intent.putExtra(Constants.EXTRA.IDX_BLOCK, mBlock);
-//                startActivity(intent);
-            }
-        };
-    }
 
-    private View.OnClickListener SaveData() {
-        return new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                addOrUpdateSection3();
-                Intent intent = new Intent(S3Activity.this, S4Activity.class);
-                intent.putExtra(Constants.EXTRA.IDX_BLOCK, mBlock);
-                intent.putExtra(Constants.EXTRA.IDX_HOUSE, section1);
-                startActivity(intent);
-                finish();
-            }
-        };
-    }
+        /////TODO CHECKS////////////////////////////
 
-    private void addOrUpdateSection3() {
-        List<Section3> list = getSection3List();
-        if (resumeModel == null) {
-            for(Section3 section3 : list){
-                section3.created_time = getTimeNowwithSeconds();
-                section3.userid = getCurrentUser().getID();
-                section3.sid = settings.getLong(Constants.SID, 0);
-                section3.setupDataIntegrity();
-                try {
-                    dbHandler.insertOrThrow(section3);
-                } catch (Exception e) {
-                    Toast.makeText(getApplicationContext(), "Exception on Section3 Insert: " + e.getMessage(), Toast.LENGTH_LONG).show();
-                }
-            }
-        } else {
-            updateSection3List(resumeModel);
-            for(Section3 s3 : resumeModel){
-                try {
-                    dbHandler.update(s3);
-                } catch (Exception e) {
-                    mUXToolkit.showToast("Update Error: " + e.getMessage());
-                }
+
+        List<Long> iid = dbHandler.replace(list);
+        for(Long i:iid){
+            if (i != null && i < 0) {
+                mUXToolkit.showToast("Failed");
+                sbtn.setEnabled(true);
+                return;
             }
         }
+        mUXToolkit.showToast("Success");
+        sbtn.setEnabled(true);
+        btnn.callOnClick();
     }
 
-    private List<Section3> getSection3List() {
-        List<Section3> list = new ArrayList<>();
-        for (int i = 301; i < 308; i++) {
-            Section3 section3 = new Section3();
-            section3.code = String.valueOf(i);
-            for(String txtId : txtIds){
-                EditText et = (EditText) findViewById(getResources().getIdentifier(txtId+i, "id", getPackageName()));
-                switch (txtId){
-                    case "Male":
-                        section3.male = Utils.GetInteger(et.getText().toString());
-                        break;
-                    case "Female":
-                        section3.female = Utils.GetInteger(et.getText().toString());
-                        break;
-                    case "Wage":
-                        section3.wages = Utils.GetInteger(et.getText().toString());
-                        break;
-                    case "Cash":
-                        section3.other_cash_payment = Utils.GetInteger((et.getText().toString()));
-                        break;
-                    case "kind":
-                        section3.payment_in_kind = Utils.GetInteger((et.getText().toString()));
-                        break;
-                }
-            }
-            section3.uid = section1.uid;
-            section3.status = 2;
-            list.add(section3);
-        }
-        return list;
-    }
-    private void updateSection3List(List<Section3> list) {
-        for(Section3 s3 : list){
-            for(String txtId : txtIds){
-                EditText et = (EditText) findViewById(getResources().getIdentifier(txtId+s3.code, "id", getPackageName()));
-                switch (txtId){
-                    case "Male":
-                        s3.male = Utils.GetInteger(et.getText().toString());
-                        break;
-                    case "Female":
-                        s3.female = Utils.GetInteger(et.getText().toString());
-                        break;
-                    case "Wage":
-                        s3.wages = Utils.GetInteger((et.getText().toString()));
-                        break;
-                    case "Cash":
-                        s3.other_cash_payment = Utils.GetInteger((et.getText().toString()));
-                        break;
-                    case "kind":
-                        s3.payment_in_kind = Utils.GetInteger((et.getText().toString()));
-                        break;
-                }
-            }
-            s3.userid = getCurrentUser().getID();
-            s3.sid = settings.getLong(Constants.SID, 0);
-            s3.modified_time = getTimeNowwithSeconds();
-            s3.sync_time = null;
-            s3.uid = section1.uid;
-            s3.status = 2;
-        }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        loadForm();
     }
 
-    private View.OnClickListener MoveToPrevious() {
-        return new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), S2Activity.class);
-                intent.putExtra(Constants.EXTRA.IDX_BLOCK, mBlock);
-                intent.putExtra(Constants.EXTRA.IDX_HOUSE, section1);
-                //Section2 s2 = dbHandler.querySingle(Section2.class, "section1Id=? and establishmentUid=?", section1.id.toString(), section1.uuid);
-                //intent.putExtra(Constants.EXTRA.IDX_SECTION2, s2);
-                startActivity(intent);
-            }
-        };
+
+    private void loadForm(){
+        modelDatabase= dbHandler.query(Section3.class," uid='"+resumeModel.uid+"' AND (is_deleted=0 OR is_deleted is null)");
+        for(Section3 s: modelDatabase){
+            setFormFromModel(this, s, inputValidationOrder, s.code, false, this.findViewById(android.R.id.content));
+        }
+
     }
 }
